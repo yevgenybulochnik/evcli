811b9295926fd9eaf7c90a1beab354c8889c6b44
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-07-31 18:23:26 -0700

    Create vm WIP

--- core/create.go
+++ core/create.go
@@ -2,8 +2,39 @@ package core
 
 import (
 	"fmt"
+	"libvirt.org/libvirt-go-xml"
 )
 
 func CreateVm() {
-	fmt.Println("Lets create a vm")
+	conn := Connect()
+
+	domcfg := &libvirtxml.Domain{
+		Type: "kvm",
+		Name: "testvm",
+		Memory: &libvirtxml.DomainMemory{
+			Value: 4096,
+			Unit:  "MB",
+		},
+		VCPU: &libvirtxml.DomainVCPU{
+			Value: 1,
+		},
+		OS: &libvirtxml.DomainOS{
+			Type: &libvirtxml.DomainOSType{
+				Arch: "x86_64",
+				Type: "hvm",
+			},
+		},
+	}
+
+	xml, err := domcfg.Marshal()
+	if err != nil {
+		panic(err)
+	}
+
+	dom, err := conn.DomainDefineXML(xml)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(dom.GetName())
 }
