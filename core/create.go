677e265af2a49fecf27d121db8767a7525311db2
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-08-01 19:48:15 -0700

    Working setup to create vm based of already create img

--- core/create.go
+++ core/create.go
@@ -24,12 +24,61 @@ func CreateVm() {
 				Type: "hvm",
 			},
 		},
+		Devices: &libvirtxml.DomainDeviceList{
+			Disks: []libvirtxml.DomainDisk{
+				{
+					Device: "disk",
+					Driver: &libvirtxml.DomainDiskDriver{
+						Name: "qemu",
+						Type: "qcow2",
+					},
+					Source: &libvirtxml.DomainDiskSource{
+						File: &libvirtxml.DomainDiskSourceFile{
+							File: "/home/yevgeny/workspace/evcli/testvm/testvm.img",
+						},
+					},
+					Target: &libvirtxml.DomainDiskTarget{
+						Dev: "vda",
+						Bus: "virtio",
+					},
+				},
+				{
+					Device: "cdrom",
+					Driver: &libvirtxml.DomainDiskDriver{
+						Name: "qemu",
+					},
+					Source: &libvirtxml.DomainDiskSource{
+						File: &libvirtxml.DomainDiskSourceFile{
+							File: "/home/yevgeny/workspace/evcli/testvm/testvm.iso",
+						},
+					},
+					Target: &libvirtxml.DomainDiskTarget{
+						Dev: "hdd",
+						Bus: "ide",
+					},
+				},
+			},
+			Serials: []libvirtxml.DomainSerial{
+				{
+					Target: &libvirtxml.DomainSerialTarget{},
+				},
+			},
+			Consoles: []libvirtxml.DomainConsole{
+				{
+					Target: &libvirtxml.DomainConsoleTarget{
+						Type: "serial",
+					},
+				},
+			},
+		},
 	}
 
 	xml, err := domcfg.Marshal()
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println(xml)
 
 	dom, err := conn.DomainDefineXML(xml)
 	if err != nil {
