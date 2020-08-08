459335646111d815560a657bb6ca1617b79f9559
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-08-08 11:14:41 -0700

    Arg and flag validation create_vm command WIP

--- cmd/create_vm.go
+++ cmd/create_vm.go
@@ -1,17 +1,37 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/spf13/cobra"
 	"github.com/yevgenybulochnik/evcli/core"
 )
 
 var createVmCmd = &cobra.Command{
-	Use: "vm",
+	Use:   "vm",
+	Short: "Create virtual machine",
+	Long:  "Create virtual machine based on profile",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return errors.New("Please provide a vm name")
+		}
+		profile_flag, _ := cmd.Flags().GetString("profile")
+
+		if !core.ProfileExists(profile_flag) {
+			return errors.New("This profile does not exist")
+		}
+
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		core.CreateVm()
+		// core.CreateVm()
+		fmt.Println("test")
 	},
 }
 
 func init() {
 	createCmd.AddCommand(createVmCmd)
+	createVmCmd.Flags().StringP("profile", "p", "", "Use configured profile")
+	createVmCmd.MarkFlagRequired("profile")
 }
