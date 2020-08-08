8328e9a39f0068ba5d0245eb8879b6787d398fdf
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-08-08 09:13:11 -0700

    GetProfilesFile method, adjust formating

--- core/utils.go
+++ core/utils.go
@@ -1,47 +1,58 @@
 package core
 
 import (
-    "fmt"
-    "bufio"
-    "os"
-    "strings"
-    "path/filepath"
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func AskForConfirmation(s string, tries int) bool {
-    reader := bufio.NewReader(os.Stdin)
-    for ; tries > 0; tries-- {
-        fmt.Printf("%s [y/n]: ", s)
+	reader := bufio.NewReader(os.Stdin)
+	for ; tries > 0; tries-- {
+		fmt.Printf("%s [y/n]: ", s)
 
-        response, err := reader.ReadString('\n')
-        if err != nil {
-            panic(err)
-        }
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
 
-        response = strings.ToLower(strings.TrimSpace(response))
+		response = strings.ToLower(strings.TrimSpace(response))
 
-        if response == "y" || response == "yes" {
-            return true
-        } else if response == "n" || response == "no" {
-            return false
-        } else {
-            fmt.Println("Invalid entry")
-        }
-    }
-    return false
+		if response == "y" || response == "yes" {
+			return true
+		} else if response == "n" || response == "no" {
+			return false
+		} else {
+			fmt.Println("Invalid entry")
+		}
+	}
+	return false
 }
 
 func CheckOrCreateConfigDir() {
-    user_home, _ := os.UserHomeDir()
-    config_dir := filepath.Join(user_home, ".evcli")
+	user_home, _ := os.UserHomeDir()
+	config_dir := filepath.Join(user_home, ".evcli")
+	profiles_file := filepath.Join(config_dir, "profiles.yaml")
 
-    if _, err := os.Stat(config_dir); os.IsNotExist(err) {
-        accept := AskForConfirmation("Evcli config dir not found would you like to create it?", 3)
-        if accept {
-            os.Mkdir(config_dir, 0755)
-        } else {
-            fmt.Println("Exiting")
-            os.Exit(0)
-        }
-    }
+	if _, err := os.Stat(config_dir); os.IsNotExist(err) {
+		accept := AskForConfirmation("Evcli config dir not found would you like to create it?", 3)
+		if accept {
+			os.Mkdir(config_dir, 0755)
+			os.Create(profiles_file)
+		} else {
+			fmt.Println("Exiting")
+			os.Exit(0)
+		}
+	}
+}
+
+func GetProfilesFile() ([]byte, string) {
+	user_home, _ := os.UserHomeDir()
+	config_dir := filepath.Join(user_home, ".evcli")
+	profiles_file := filepath.Join(config_dir, "profiles.yaml")
+	file, _ := ioutil.ReadFile(profiles_file)
+	return file, profiles_file
 }
