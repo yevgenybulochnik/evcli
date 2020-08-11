ce0520fc6c973d7fff314e4eae1aec355fe73a77
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-08-10 17:23:34 -0700

    Create meta-data given struct

--- core/metadata.go
+++ core/metadata.go
@@ -1,1 +1,26 @@
 package core
+
+import (
+    "bytes"
+    "io/ioutil"
+    "os"
+
+    "gopkg.in/yaml.v3"
+)
+
+type MetaData struct {
+    InstanceId string `yaml:"instance-id"`
+    LocalHostname string `yaml:"local-hostname"`
+}
+
+func (metaData *MetaData) generateFile() *os.File {
+    var data bytes.Buffer
+
+    encoder := yaml.NewEncoder(&data)
+    encoder.SetIndent(2)
+    encoder.Encode(&metaData)
+
+    file, _ := ioutil.TempFile("", "meta-data")
+    file.Write(data.Bytes())
+    return file
+}
