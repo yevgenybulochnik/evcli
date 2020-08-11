3c92125c3a699f7b2bfe9c60286ca7e139a19931
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-08-10 17:22:41 -0700

    Create network-config given struct

--- core/networkconfig.go
+++ core/networkconfig.go
@@ -1,1 +1,30 @@
 package core
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+
+    "gopkg.in/yaml.v3"
+)
+
+type Ethernet struct {
+    Dhcp4 bool `yaml:"dhcp4"`
+}
+
+type NetworkConfig struct {
+    Version int `yaml:"version"`
+    Ethernets map[string]Ethernet `yaml:"ethernets"`
+}
+
+func (networkConfig *NetworkConfig) generateFile() *os.File {
+	var data bytes.Buffer
+
+	encoder := yaml.NewEncoder(&data)
+	encoder.SetIndent(2)
+	encoder.Encode(&networkConfig)
+
+	file, _ := ioutil.TempFile("", "network-config")
+	file.Write(data.Bytes())
+	return file
+}
