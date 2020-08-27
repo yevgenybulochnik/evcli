d79029ad00c4f0c2666638b7878716736e7955f9
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-08-26 20:20:13 -0700

    Add ssh field, adjust user struct

--- core/cloudinit.go
+++ core/cloudinit.go
@@ -9,17 +9,20 @@ import (
 )
 
 type User struct {
-	Name       string `yaml:"name"`
-	Groups     string `yaml:"groups,omitempty"`
-	LockPasswd bool   `yaml:"lock_passwd"`
-	Passwd     string `yaml:"passwd,omitempty"`
-	Shell      string `yaml:"shell"`
+	Name        string   `yaml:"name"`
+	Groups      string   `yaml:"groups,omitempty"`
+	Sudo        []string `yaml:"sudo,omitempty"`
+	LockPasswd  bool     `yaml:"lock_passwd"`
+	Passwd      string   `yaml:"passwd,omitempty"`
+	Shell       string   `yaml:"shell"`
+	SshAuthKeys []string `yaml:"ssh_authorized_keys,omitempty"`
 }
 
 type CloudConfig struct {
-	Hostname         string `yaml:"hostname"`
-	PreserveHostname bool   `yaml:"preserve_hostname"`
-	Users            []User `yaml:"users"`
+	Hostname         string   `yaml:"hostname"`
+	PreserveHostname bool     `yaml:"preserve_hostname"`
+	Users            []User   `yaml:"users,omitempty"`
+	SshAuthKeys      []string `yaml:"ssh_authorized_keys,omitempty"`
 }
 
 func (cloudConfig *CloudConfig) generateFile(tempDir string) *os.File {
